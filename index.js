9785107a62ae09263ea53c47907485be207b840c
yet1009 <84160197+yet1009@users.noreply.github.com>
2022-11-08 22:02:22 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
 
-mongoose.connect('mongodb+srv://yet1009:abcd1234@cluster0.xuoetcg.mongodb.net/?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MogoDB Connected')).catch(err => console.log(err))
@@ -12,6 +20,23 @@ mongoose.connect('mongodb+srv://yet1009:abcd1234@cluster0.xuoetcg.mongodb.net/?r
 // 
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!~~~~~~~~!'));
+
+app.post('/register',(req, res) => {
+
+    //회원가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
